d40ec8430bd6c422319ded66c2fdaffcbf65958e
Amine Mahjoub <31483078+eminemahjoub@users.noreply.github.com>
2023-01-27 22:25:26 +0100

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,5 +1,4 @@
-// NOTE! The steps in this file are basically identical to the ones you
-// performed in the SpeakHello.js file.
+
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
